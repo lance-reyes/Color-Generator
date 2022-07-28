c6f23a5406a6ca03216c221b264a98076c83dc88
Lance Reyes <lrandreireyes@gmail.com>
2022-07-28 20:56:28 +0800

    added files via upload
    I added a HEX code feature.

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const button = document.querySelector('button');
-const h1 = document.querySelector('h1');
+const rgb = document.querySelector('#rgb');
+const hex = document.querySelector('#hex');
 
 button.addEventListener('click', () => {
     const newColor = makeRandomColor();
-    document.body.style.backgroundColor = newColor;
-    h1.innerText = newColor;
+    document.body.style.backgroundColor = newColor[0];
+    rgb.innerText = newColor[0];
+    hex.innerText = newColor[1];
 })
 
 const makeRandomColor = () => {
@@ -12,5 +14,12 @@ const makeRandomColor = () => {
     const g = Math.floor(Math.random() * 255);
     const b = Math.floor(Math.random() * 255);
 
-    return `rgb(${r}, ${g}, ${b})`;
+    const rgb = `rgb(${r}, ${g}, ${b})`;
+    const hex = rgbToHex(r, g, b);
+    return [rgb, hex];
 }
+
+const rgbToHex = (r, g, b) => {
+    return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+}
+
